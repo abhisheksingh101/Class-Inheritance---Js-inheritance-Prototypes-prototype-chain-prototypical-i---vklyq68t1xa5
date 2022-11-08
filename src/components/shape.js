6100e2ed5e1baaae48ed624eaef6efc142915b0e
Abhishek Singh <70930933+abhisheksingh101@users.noreply.github.com>
2022-11-08 09:50:45 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
 
-// export class using module.exports
+class Shape {
+  constructor() {
+    this.color = "#FF40FF";
+    this.state = () => {
+      return 5;
+    };
+  }
+  drawShape() {
+    return this.color;
+  }
+  calculateArea() {
+    return 1;
+  }
+}
+
+module.exports = Shape;
+
